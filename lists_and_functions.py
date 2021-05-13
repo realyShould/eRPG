9c0cf437fc38add20eda923e1eb16c31cb6e6130
Nikita <benjamin.engel@inbox.ru>
2021-05-13 21:18:26 +0700

    Add classes

---
+++ lists_and_functions.py
@@ -0,0 +1,17 @@
+import random, os, colorama
+from colorama import Fore, Back, Style
+
+colorama.init()
+
+class Player():
+    def __init__(self):
+        pass
+
+class Wolf():
+    def __init__(self):
+        pass
+
+class Bear():
+    def __init__(self):
+        pass
+
---
+++ main.py
@@ -0,0 +1,5 @@
+from colorama import Fore, Back, Style
+import colorama
+colorama.init()
+
+print(f'{Fore.YELLOW}\t\t█▀▀█ █▀▀█ █▀▀█\n\t\t█▄▄▀ █░░█ █░░▄▄\n\t\t▀░▀▀ █▀▀▀ █▄▄█')
