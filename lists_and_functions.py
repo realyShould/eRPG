35ce7cd726aca755696db724f92ad2d5b38061f5
Nikita <benjamin.engel@inbox.ru>
2021-05-13 23:09:14 +0700

    Upgrade

--- lists_and_functions.py
+++ lists_and_functions.py
@@ -5,12 +5,12 @@ colorama.init()
 
 class Player():
     def __init__(self):
-        self.right_now_hp = self.default_hp + self.armor
         self.default_hp = 100
         self.armor = 0
-        self.right_now_atk = self.default_atk + self.additional_atk
+        self.right_now_hp = self.default_hp + self.armor
         self.default_atk = 20
         self.additional_atk = 0
+        self.right_now_atk = self.default_atk + self.additional_atk
         self.coins = 0
         self.heal_count = 0
         self.heal_atr = 50
@@ -45,10 +45,11 @@ class Wolf():
         if player.right_now_hp <= 0:
             print(f'\tLose!')
             player.__init__()
+            userCommand(player, wolf, bear)
         self.fight(player)
 
 class Bear():
-    def __init__(self):
+    def __init__(self, player):
         self.hp = 50
         self.right_now_hp = self.hp
         self.atk = 20
@@ -63,7 +64,14 @@ class Bear():
         if player.right_now_hp <= 0:
             print(f'\tLose!')
             player.__init__()
+            userCommand(player, wolf, bear)
         self.fight(player)
+
+player = Player()
+wolf = Wolf(player)
+bear = Bear(player)
+
+
 
 shopItems=[
     'Easy Armor',
@@ -99,3 +107,143 @@ listOfEnemy=[
     'wolf',
     'bear'
 ]
+
+def random_opponent(wolf, player, bear):
+    enemy = random.choice(listOfEnemy)
+    if enemy == 'wolf':
+        wolf.fight(player)
+        userCommand(player, wolf, bear)
+    else:
+        bear.fight(player)
+        userCommand(player, wolf, bear)
+
+def shop(player):
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print(f'[1]',shopItems[0],' ',str(shopItemsPrice[shopItems[0]]))
+    print(f'[2]',shopItems[1],' ',str(shopItemsPrice[shopItems[1]]))
+    print(f'[3]',shopItems[2],' ',str(shopItemsPrice[shopItems[2]]))
+    print(f'[4]',shopItems[3],' ',str(shopItemsPrice[shopItems[3]]))
+    print(f'[5]',shopItems[4],' ',str(shopItemsPrice[shopItems[4]]))
+    print(f'[6]',shopItems[5],' ',str(shopItemsPrice[shopItems[5]]))
+    print(f'[7]',shopItems[6],' ',str(shopItemsPrice[shopItems[6]]))
+    print(f'or [exit]')
+    userShop=input('shop:')
+    userShop=userShop.strip()
+    userShop=userShop.lower()
+    if userShop=='':
+        shop()
+    elif userShop=='exit':
+        userCommand(player)
+    elif userShop == '1' and player.coins >= shopItemsPrice[shopItems[0]]:
+        if 'Easy Armor' in player.armor_inventory:
+            shop()
+        else:
+            player.armor_inventory.append(str(shopItems[int(userShop)-1]))
+            player.coins-=shopItemsPrice[shopItems[0]]
+            player.updateStatus_f()
+            shop()
+    elif userShop == '2' and player.coins >= shopItemsPrice[shopItems[1]]:
+        if 'Medium Armor' in player.armor_inventory:
+            shop()
+        else:
+            player.armor_inventory.append(str(shopItems[int(userShop)-1]))
+            player.coins-=shopItemsPrice[shopItems[0]]
+            player.updateStatus_f()
+            shop()
+    elif userShop == '3' and player.coins >= shopItemsPrice[shopItems[2]]:
+        if 'Hard Armor' in player.armor_inventory:
+            shop()
+        else:
+            player.armor_inventory.append(str(shopItems[int(userShop)-1]))
+            player.coins-=shopItemsPrice[shopItems[0]]
+            player.updateStatus_f()
+            shop()
+    elif userShop == '4' and player.coins >= shopItemsPrice[shopItems[3]]:
+        player.healC+=1
+        player.coins-=shopItemsPrice[shopItems[0]]
+        player.updateStatus_f()
+        shop()
+    elif userShop == '5' and player.coins >= shopItemsPrice[shopItems[4]]:
+        if 'Wooden Sword' in player.weapon_inventory:
+            shop()
+        else:
+            player.weapon_inventory.append(str(shopItems[int(userShop)-1]))
+            player.coins-=shopItemsPrice[shopItems[0]]
+            player.updateStatus_f()
+            shop()
+    elif userShop == '6' and player.coins >= shopItemsPrice[shopItems[5]]:
+        if 'Stone Sword' in player.weapon_inventory:
+            shop()
+        else:
+            player.weapon_inventory.append(str(shopItems[int(userShop)-1]))
+            player.coins-=shopItemsPrice[shopItems[0]]
+            player.updateStatus_f()
+            shop()
+    elif userShop == '7' and player.coins >= shopItemsPrice[shopItems[6]]:
+        if 'Iron Sword' in player.weapon_inventory:
+            shop()
+        else:
+            player.weapon_inventory.append(str(shopItems[int(userShop)-1]))
+            player.coins-=shopItemsPrice[shopItems[0]]
+            player.updateStatus_f()
+            shop()
+    else:
+        print(f'\tError. Restart!')
+        player.updateStatus_f()
+        shop()
+
+def hunt_f(player, wolf, bear):
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print('\n\tWho is your enemy?')
+    print('[1]Wolf\n[2]Bear\n[rand]Random(dont work)')
+    opponent=input('input:').strip().lower()
+    if opponent == '':
+        hunt_f()
+    elif opponent == '1':
+        wolf.fight(player)
+    elif opponent == '2':
+        bear.fight(player)
+    elif opponent == 'rand':
+        random_opponent(wolf, player, bear)
+        userCommand(player, wolf, bear)
+    else:
+        print('Such an opponent does not exist or there was an error in the code, for the second option, contact the developer. Repeat.')
+        hunt_f()
+    userCommand(player)
+
+def userCommand(player, wolf, bear):
+    ##os.system('cls' if os.name == 'nt' else 'clear')
+    print(Fore.RED,'\t\tHelper')
+    print(Fore.CYAN,'hunt - hunting')
+    print(Fore.CYAN,'shop - shop')
+    print(Fore.CYAN,'heal - heal your character')
+    print(Fore.CYAN,'inventory or inv - inventory')
+    print(Fore.CYAN,'stats')
+    print(Fore.CYAN,'exit')
+    print(Fore.MAGENTA)
+    userCommand=input('input:').strip().lower()
+    if userCommand == '':
+        userCommand()
+    elif userCommand == 'shop':
+        shop()
+    elif userCommand == 'hunt':
+        hunt_f(player, wolf, bear)
+    elif userCommand=='stats' or userCommand == 'status':
+        player.output_status()
+        userCommand()
+    elif userCommand=='exit':
+        quit()
+    elif userCommand=='inventory' or userCommand == 'inv':
+        print(Style.RESET_ALL,Fore.CYAN,'\tYour armor:\n')
+        print(player.armor_inventory)
+        print('\n\tYour weapons:\n')
+        print(player.weapon_inventory)
+        print('\n\tHealth poitions:')
+        print(str(player.heal_count))
+        userCommand()
+    elif userCommand == 'heal':
+        player.heal()
+        userCommand()
+    else:
+        print('You entered the wrong command or there was an error in your code. In the case of the latter, contact the developer. Restart.')
+        userCommand() 
--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from colorama import Fore, Back, Style
-import colorama
+import colorama, os, random
+import lists_and_functions as f
+
 colorama.init()
 
+
 print(f'{Fore.YELLOW}\t\t█▀▀█ █▀▀█ █▀▀█\n\t\t█▄▄▀ █░░█ █░░▄▄\n\t\t▀░▀▀ █▀▀▀ █▄▄█')
+
+f.userCommand(f.player, f.wolf, f.bear)
